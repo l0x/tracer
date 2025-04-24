4663902f935c5c40104e8833413993495d3d4f64
Sebastian Clarke <seb@penrodyn.com>
2025-04-24 15:18:42 +0200

    move prims to own module

--- scene.go
+++ scene.go
@@ -34,32 +34,3 @@ func (scene *Scene) GetBackground(ray *Ray) *vec.Vec3 {
 	// but we could use the ray to make a gradient...
 	return &scene.background
 }
-
-type Sphere struct {
-	origin   vec.Vec3
-	radius   float64
-	material *Material
-}
-
-func (s *Sphere) Intersects(ray *Ray) (bool, *Intersection) {
-	fromCenter := ray.pos.Sub(s.origin)
-	dist := fromCenter.Magnitude()
-
-	// if fromCenter := s.origin.Sub(ray.pos); fromCenter.Magnitude() <= s.radius {
-	if dist <= s.radius {
-		return true, &Intersection{fromCenter.Norm(), ray.pos, s.material}
-	}
-	return false, nil
-}
-
-type Plane struct {
-	height   float64
-	material *Material
-}
-
-func (p *Plane) Intersects(ray *Ray) (bool, *Intersection) {
-	if ray.pos.Y <= p.height {
-		return true, &Intersection{vec.Vec3{X: 0, Y: 1, Z: 0}, ray.pos, p.material}
-	}
-	return false, nil
-}
